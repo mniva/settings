ecb6d37e3f1e0e046fa8301d287624443a1e1ac9
Mikael Niva <mikael.niva@monterosa.se>
2017-11-22 22:17:14 +0100

    works

--- .emacs.d/lisp/mn-org.el
+++
@@ -1,7 +0,0 @@
-;; Org-mode settings
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-font-lock-mode 1)
-
-(provide 'mn-org)
