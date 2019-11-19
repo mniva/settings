4b8e91fa0fad35e1431a6d1c2289436447fa93d6
Mikael Niva <nivamikael@gmail.com>
2019-11-19 13:09:48 +0100

    cask install
    insomnia

--- brew_installation.sh
+++ brew_installation.sh
@@ -4,7 +4,7 @@
 brew install ruby emacs yarn node jq catj
 
 # Casks
-brew cask install alfred sketch dropbox franz authy
+brew cask install alfred sketch dropbox franz authy insomnia
 
 # Drivers (hacky still)
 # brew tap homebrew/cask-drivers
