198af41fc402dc93eaea034941949567b6b981f9
Expressen Android CI build robot <34092402+exp-android@users.noreply.github.com>
2020-02-17 09:57:39 +0100

    Update brew_installation.sh
    added cask iterm

--- brew_installation.sh
+++ brew_installation.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Packages
-brew install ruby emacs yarn node jq catj
+brew install ruby emacs node yarn jq catj
 
 # Casks
-brew cask install alfred sketch dropbox franz authy insomnia
+brew cask install alfred sketch dropbox franz authy iterm
 
 # Drivers (hacky still)
 # brew tap homebrew/cask-drivers
